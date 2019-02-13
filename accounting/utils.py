afc481e2147f27e523b3e30514884e711ab23a56
susiyanti <susiyanti.g11@gmail.com>
2019-02-13 14:17:28 +0700

    6. Policy named_insured field should not be nullable

--- accounting/utils.py
+++ accounting/utils.py
@@ -192,6 +192,7 @@ def insert_data():
     policies = []
     p1 = Policy('Policy One', date(2015, 1, 1), 365)
     p1.billing_schedule = 'Annual'
+    p1.named_insured = john_doe_insured.id
     p1.agent = bob_smith.id
     policies.append(p1)
 
